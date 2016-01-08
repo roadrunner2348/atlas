24c638b7c0270e453d7004ec09878fb18da508f1
Justin Wolf <jwolf@keansburg.k12.nj.us>
2016-01-08 16:21:47 -0500

    Started JSS Write API Integration

--- atlas/JSSLabelWriter/views.py
+++ atlas/JSSLabelWriter/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import requests
 
 @login_required
 def index(request):
     return render(request, 'JSSLabelWriter/index.html', {})
+
+@login_required
+def jss_device_search(request):
+
+    r = requests.get('https://jss-client.keansburg.k12.nj.us:8443/JSSResource/computers/subset/basic')
+    return render(request, 'JSSLabelWriter/index.html', {'r':r})
