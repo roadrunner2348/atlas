229418461430bf6b82331913b60bb3264ede291d
Justin Wolf <jwolf@keansburg.k12.nj.us>
2016-01-14 15:08:12 -0500

    Fixed Printer Instructions Template Bug

--- atlas/JSSLabelWriter/views.py
+++ atlas/JSSLabelWriter/views.py
@@ -42,7 +42,7 @@ def label(request, device_id):
         return render(request, 'JSSLabelWriter/label.html', {'message':message})
 @login_required
 def printer_instructions(request):
-    return render(request, 'jsslabelwriter/printer-instructions.html', {})
+    return render(request, 'JSSLabelWriter/printer-instructions.html', {})
 
 def get_jss_data(url):
 
