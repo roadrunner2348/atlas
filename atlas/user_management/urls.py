2109cabd5130a0b73903195b61206ce46c22cd54
Justin Wolf <roadrunner2348@gmail.com>
2016-02-03 02:01:28 -0500

    Login UI Changes

--- atlas/user_management/urls.py
+++ atlas/user_management/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+app_name = "user_management"
 urlpatterns = [
     url(r'^login', views.user_login, name = 'login'),
     url(r'^logout', views.user_logout, name = 'logout'),
