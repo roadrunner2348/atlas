6f3f7e22accb76f429fc97240aadfc33ba2fa653
Justin Wolf <jwolf@keansburg.k12.nj.us>
2016-02-05 16:15:46 -0500

    Changed user and tag form to force all lowercase

--- atlas/static/js/atlas.js
+++ atlas/static/js/atlas.js
@@ -2,3 +2,7 @@ str = $(".barcode-39").first().text();
 $(".barcode-39").barcode( str, "code39", {barHeight:20, fontSize:14});
 
 $(".chosen-select").chosen();
+
+$(".tolowercase").keyup(function () {
+  this.value = this.value.toLowerCase();
+});
